d81b4ee62dbecccc9195120b3272d2c68f26db88
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-02 10:28:08 +0300

    added a div for home title and text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='home-page'>
+    <div className="home-page">
+      <div className="home-main">
+        <h1 className="home-title">Embrace the Adventure</h1>
+        <p className="home-text">Choose Your Favourite Destination</p>
+      </div>
       
     </div>
-  )
+  );
 }
 
 export default Home
