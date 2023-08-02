1f4258352077f0e4cc19c43238e6e14bd1ec6980
AdrianMunandi <adrian@ex.example.com>
2023-08-02 20:29:15 +0300

    Updated

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 
 class ContactForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     alert('Message submitted!');
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
+    const { name, email, message } = this.state;
+
     return (
       <div>
-        <h1>Send a message to us!</h1>
-      <form className="contact-form" onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <h1 class="header">Send a message to us!</h1>
+        <form className="contact-form" onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={name} onChange={this.handleChange} />
 
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={email} onChange={this.handleChange} />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" />
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" value={message} onChange={this.handleChange} />
 
-        <button type="submit">Send Message</button>
-      </form>
+          <button type="submit">Send Message</button>
+        </form>
       </div>
     );
   }
