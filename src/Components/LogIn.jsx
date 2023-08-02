f3de6d730ff182d3860d8a39a7216126d6c8c62b
Noah <noah.ntabo@student.moringaschool.com>
2023-08-02 17:45:50 +0300

    Image style added to the page

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import "./LogIn.css"
 
 function LogIn(){
 
     return (
-        <div>
-            <h2>Log In</h2>
-            <form>
-                <label>Email:</label>
-                <input type="email" name="email" required/>
-                <br/>
-                <label>Password:</label>
-                <input type="password" name="password" required/>
-                <br/>
-                <button type="submit">Log In</button>
-            </form>
+        <div className="logincontainer">
+            <div className="loginimage">
+                <h2 className="imageheading">Log In</h2>
+            </div>
+            <div className="loginform">
+                <form>
+                    <label>Email:</label>
+                    <input type="email" name="email" required/>
+                    <br/>
+                    <label>Password:</label>
+                    <input type="password" name="password" required/>
+                    <br/>
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+           
+           
         </div>
     )
 }
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import "./SignUp.css"
 
 function SignUp(){
 
     return (
-        <div>
-            <h2>Sign Up</h2>
-            <form>
-                <label>Name:</label>
-                <input type="text" name="name" required/>
-                <br/>
-                <label>Email:</label>
-                <input type="email" name="email" required/>
-                <br/>
-                <label>Password:</label>
-                <input type="password" name="password" required/>
-                <br/>
-                <button type="submit">Sign Up</button>
-            </form>
-            <p>Already have an account?<a href="/login">Log In</a></p>
+        <div className="signupcontainer">
+            <div className="signupimage">
+                <h2 className="signupheading">Sign Up</h2>
+            </div>
+            <div className="signupform">
+                <h2>Sign Up</h2>
+                <form>
+                    <label>Name:</label>
+                    <input type="text" name="name" required/>
+                    <br/>
+                    <label>Email:</label>
+                    <input type="email" name="email" required/>
+                    <br/>
+                    <label>Password:</label>
+                    <input type="password" name="password" required/>
+                    <br/>
+                    <button type="submit">Sign Up</button>
+                </form>
+                <p>Already have an account?<a href="/login">Log In</a></p>
+            </div>
+            
+            
         </div>
     )
 }
