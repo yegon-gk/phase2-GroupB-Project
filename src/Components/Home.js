6725ebc5aea6004a5f554b4272046dc6f5228fbe
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-02 10:30:17 +0300

    added a div for home content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,11 @@ function Home() {
         <h1 className="home-title">Embrace the Adventure</h1>
         <p className="home-text">Choose Your Favourite Destination</p>
       </div>
+
+      <div className="home-content">
+        <h1>Popular Destination</h1>
+        <p>Tours give you the opportunity to see a lot, within a time frame</p>
+      </div>
       
     </div>
   );
