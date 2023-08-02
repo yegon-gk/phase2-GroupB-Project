431456a94bd94f029d53915dfb1db8e9956b5fdc
Noah <noah.ntabo@student.moringaschool.com>
2023-08-02 18:32:53 +0300

    Final css

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -9,6 +9,7 @@ function LogIn(){
                 <h2 className="imageheading">Log In</h2>
             </div>
             <div className="loginform">
+                <h2>Log In</h2>
                 <form>
                     <label>Email:</label>
                     <input type="email" name="email" required/>
