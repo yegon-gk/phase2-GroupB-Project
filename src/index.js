96995ff08ed1c6d76294f096bd31dfcd60e9847b
AdrianMunandi <adrian@ex.example.com>
2023-08-03 11:47:06 +0300

    almost done resolving conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import ContactForm from './Components/ContactForm';
 import './Components/Contact.css';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setName('');
+      setEmail('');
+      setMessage('');
+    }, 1000);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'message') {
+      setMessage(value);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="header">Send a message to us!</h1>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label id="name">Name:</label>
+        <input type="text" id="name" name="name" value={name} onChange={handleChange} />
+
+        <label id="email">Email:</label>
+        <input type="email" id="email" name="email" value={email} onChange={handleChange} />
+
+        <label id="message">Message:</label>
+        <textarea id="message" name="message" value={message} onChange={handleChange} />
+
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -17,3 +61,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
