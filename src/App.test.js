a7fc050124a001ab0b0e02715c65a74d28509f73
AdrianMunandi <adrian@ex.example.com>
2023-08-03 11:13:26 +0300

    Updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
+// Import the necessary testing libraries
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import MyComponent from './MyComponent'; // Replace with your component path
+
+test('renders the component with the correct text content', () => {
+  render(<MyComponent />);
+  const element = screen.getByText(/Hello, World/i);
+  expect(element).toBeInTheDocument();
 });
-
