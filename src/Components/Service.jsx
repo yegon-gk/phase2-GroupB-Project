b2f1ea8b30974569cd0d438e4d20c97796db7dcf
KIPROTYCH <kiprotych@gmail.com>
2023-08-03 19:15:58 +0300

    Modified these files

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -102,7 +102,7 @@ const Service = () => {
       <Navbar />
       <div className="w-full mx-auto text-center mt-20 mb-10">
         <h2 className="font-semibold text-xl tracking-wide text-blue-500 mt-5 section-title">
-          Elevate Your Accommodation Experience
+          Elevate Your Accommodation Experience Now
         </h2>
         <div className="w-full px-4 md:px-10 mt-6">
           <input
