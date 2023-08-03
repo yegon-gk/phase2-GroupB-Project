4302f654401c628a1d36cb9cbc194e396e9bcedf
AdrianMunandi <adrian@ex.example.com>
2023-08-03 11:08:37 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ContactForm from './Components/ContactForm';
+import './Components/Contact.css';
+
+function App() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,13 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// Import the necessary testing libraries
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import MyComponent from './MyComponent'; // Replace with your component path
+
+test('renders the component with the correct text content', () => {
+
+  render(<MyComponent />);
+  const element = screen.getByText(/Hello, World/i);
+  expect(element).toBeInTheDocument();
+});
 
