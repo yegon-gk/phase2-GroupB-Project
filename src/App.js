e6d2265d40b5321911be501a83aa14bb0935d32a
AdrianMunandi <adrian@ex.example.com>
2023-08-03 13:10:14 +0300

    Trying to resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ContactForm from './Components/ContactForm';
-import './Components/Contact.css';
+import Footer from './Components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <ContactForm />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div>
+        <ContactForm />
+        <Footer />
+      </div>
     </div>
   );
 };
